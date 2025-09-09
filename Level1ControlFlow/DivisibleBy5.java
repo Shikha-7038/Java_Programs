97119d54cf6e617c9a7da5d9c83dd0d9b4720237
Shikha-7038 <shikhagahlawat2@gmail.com>
2025-09-09 11:00:43 +0530

    New

---
+++ Level1ControlFlow/DivisibleBy5.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+public class DivisibleBy5 {
+     public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter a number: ");
+        int number = sc.nextInt();
+
+        if(number % 5 ==0){
+            System.out.println("Number is divisible by 5");
+        }else{
+            System.out.println("Number is not divisible by 5");
+        }
+
+        sc.close();
+    }
+}
